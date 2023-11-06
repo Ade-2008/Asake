e5efb123cb7b798cf5d200c793c6f9aea0d4d1d6
Ade-2008 <146386311+Ade-2008@users.noreply.github.com>
2023-11-06 19:33:35 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+const hamburger = document.querySelector(".hamburger");
+const nav_menu = document.querySelector(".nav-menu");
+const changeIcon = document.querySelector(".fa-bars");
+
+
+    hamburger.addEventListener("click", () =>{
+        hamburger.classList.toggle("active");
+        nav_menu.classList.toggle("active");
+        
+        if( hamburger.classList.contains("active")){
+            changeIcon.setAttribute("class", "fa-solid fa-times")
+        }
+        else{
+            changeIcon.setAttribute("class", "fa-solid fa-bar")
+        }
+
+    });
+    
+
+
+
+const quote = document.querySelector(".read");
+const btn = document.querySelector(".btn");
+const quotes =[{
+    quote:`"HE MADE ME AN UNSTOPPABLE DEVELOPER."`
+},{
+    quote:`"I LOVE DESIGNING BECAUSE AM A DEVELOPER."`
+},{
+    quote:`"I'M WEB DESIGNER. WHO LOVE CODING."`
+},{
+    quote:`"I LOVE MY BOSS SO MUCH. HIS THE BEST TEACHER EVER."`
+},{
+    quote:`"AT DESIGNING I TRY MY BEST TO SATISFY MY VIEWER."`
+}];
+
+
+btn.addEventListener('click',function(e){
+    let random = Math.floor(Math.random()*quotes.length);
+
+    quote.innerText = quotes[random].quote;
+})
